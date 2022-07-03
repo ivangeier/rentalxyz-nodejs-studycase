fb70b7bbe3faebb956108ff574822194a0f47e23
ivangeier <vilajr.ivan@gmail.com>
2022-07-03 12:00:30 -0300

    ✨ Add create category endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express, { response } from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
+//config
 const app = express();
-
 app.use(express.json());
 
-app.get('/', (request, response) => {
-   return response.json({ message: 'Hello world' });
-});
+//routes
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
    'Server is running on PORT 3333';
