6cd2d0d5ae89813a5a061f166af376a8c8232dac
ivangeier <vilajr.ivan@gmail.com>
2022-07-05 21:47:48 -0300

    ✨Add specifications endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationRoutes } from './routes/specification.routes';
 
 // config
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // routes
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationRoutes);
 
 app.listen(3333, () => {
   'Server is running on PORT 3333';
