951ab98dcd64dacf5bca83b99fdc80445b3864c5
ivangeier <vilajr.ivan@gmail.com>
2022-07-10 14:19:40 -0300

    ♻️ Implement index on router file and implement specification with usecases

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specification.routes';
+import { router } from './routes';
 
 // config
 const app = express();
 app.use(express.json());
 
 // routes
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationRoutes);
+app.use(router);
 
 app.listen(3333, () => {
   'Server is running on PORT 3333';
