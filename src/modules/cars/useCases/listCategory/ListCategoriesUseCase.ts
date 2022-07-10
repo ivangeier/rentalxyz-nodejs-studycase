732fbcd4aa222f4b84dbe8c5d3643766d9df0282
ivangeier <vilajr.ivan@gmail.com>
2022-07-09 23:13:43 -0300

    ♻️ Separate implementations

--- src/modules/cars/useCases/listCategory/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategory/ListCategoriesUseCase.ts
@@ -1,4 +1,4 @@
-import { Category } from '../../model/category';
+import { Category } from '../../model/Category';
 import { ICategoryRepository } from '../../repositories/ICategoryRepository';
 
 class ListCategoryUseCase {
